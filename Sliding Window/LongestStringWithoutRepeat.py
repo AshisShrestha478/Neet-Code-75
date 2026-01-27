9cf36aab86edda58f402aed014d989da12a2bbc3
Ashis Shrestha <shrestha7.np@gmail.com>
2026-01-27 14:49:01 -0600

    Longest Substring Without Repeating Characters

---
+++ Sliding Window/LongestStringWithoutRepeat.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def lengthOfLongestSubstring(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        charSet = set()
+        left,longest = 0,0
+
+        for right in range(len(s)):
+            while s[right] in charSet:
+                charSet.remove(s[left])
+                left += 1
+            charSet.add(s[right])
+            longest = max(longest, right-left+1)
+        
+        return longest
